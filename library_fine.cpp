8c248ef437a42f28dfb8eab9b71b4da155309c31
amrithm98 <amrithchallenger@gmail.com>
2017-08-14 23:29:01 +0530

    LibraryFine

--- library_fine.cpp
+++ library_fine.cpp
@@ -14,5 +14,16 @@ int main(){
     int m2;
     int y2;
     cin >> d2 >> m2 >> y2;
+    int fine=0;
+    int diff_d=d1-d2;
+    int diff_m=m1-m2;
+    int diff_y=y1-y2;
+    if(diff_d>0 && diff_m==0 && diff_y==0)
+        fine=15*diff_d;
+    else if(diff_m>0 && diff_y==0)
+        fine=500*diff_m;
+    else if(diff_y>0)
+        fine=10000;
+    cout<<fine<<"\n";
     return 0;
 }
