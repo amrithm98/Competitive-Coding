c15e1d46634e285034769ecd5b87411ac395613a
amrithm98 <amrithchallenger@gmail.com>
2017-10-22 09:32:30 +0530

    Assgn 4 -2nd ques

---
+++ Coding-Ninjas/Assignment-4-Number-Theory/n_factorful.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#define MAX 1000001
+using namespace std;
+
+vector<long> sieve_of_eratosthenes()
+{
+    vector<bool> sieve(MAX);
+    vector<long> divisorCount(MAX);
+
+    sieve[0] = false;
+    sieve[1] = false;
+
+    for(long i = 2; i <= MAX ; i++)
+        sieve[i] = true;
+
+    for(long i = 2 ; i <= sqrt(MAX) ; i++)
+    {
+        for(long j = i*i; j <= MAX ; j+=i)
+        {
+            sieve[j] = false;
+        }
+    }
+
+    vector<long> primes;
+    primes.push_back(2);
+
+    for(long i = 3; i <= MAX; i++)
+        if(sieve[i])
+            primes.push_back(i);
+
+    for(int i = 0; i < primes.size(); i++)
+    {
+        for(int j = 1; primes[i]*j <= MAX ; j++ )
+            divisorCount[ primes[i]*j ] ++;
+    }
+
+    return divisorCount;
+
+}
+
+int main()
+{
+	// Write your code here
+    int t;
+    cin >> t;
+
+    vector<long> sieve;
+    sieve = sieve_of_eratosthenes();
+
+    while(t--)
+    {
+        long long a,b;
+        int n;
+
+        cin >> a >> b >>n;
+
+        int n_factor = 0;
+
+        for(long i = a; i <= b; i++)
+        {
+            if(sieve[i] == n)
+                n_factor++;
+        }
+
+        cout<< n_factor << endl;
+    }
+	return 0;
+}
