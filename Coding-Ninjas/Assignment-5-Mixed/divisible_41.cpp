3442c9a1c2ac914217b153e52e3918553a819e0b
amrithm98 <amrithchallenger@gmail.com>
2017-10-28 23:06:52 +0530

    Wordbreak

--- Coding-Ninjas/Assignment-5-Mixed/divisible_41.cpp
+++ Coding-Ninjas/Assignment-5-Mixed/divisible_41.cpp
@@ -3,42 +3,6 @@
 #include <string>
 using namespace std;
 
-long long divisible41(string s)
-{
-    int len = s.length();
-    if(len < 8)
-    {
-        return (stol(s)%41 == 0);
-    }
-    if(s == "0")
-    {
-        return 0;
-    }
-
-    if(len%5 !=0)
-    {
-        int bal = ((len/5)+1)*5-len;
-        // cout<<bal<<" "<<len<<endl;
-        for(int i = 1; i <=bal ; i++)
-            s = "0"+s;
-        len += bal;
-    }
-
-    int gsum = 0;
-
-    while(len != 0)
-    {
-        string group = s.substr(len-5,5);
-        len -= 5;
-        long val = stol(group);
-        gsum += val;
-    }
-
-    if(gsum >= 100000)
-        return divisible41(to_string(gsum));
-    else
-        return (gsum%41 ==0);
-}
 
 int main()
 {
@@ -48,19 +12,28 @@ int main()
     {
         int a0,a1,c,n;
         cin >> a0 >> a1 >> c >> n;
-
+        if( n <= 1)
+        {
+            cout<<"NO"<<endl;
+            continue;
+        }
         string s = "";
         s += to_string(a0);
         s += to_string(a1);
         for(int i = 2; i < n; i++)
         {
             s += to_string(((s[i-1]-'0')*c+(s[i-2]-'0'))%10);
-
         }
-        if(divisible41(s))
-            cout<<"YES"<<endl;
-        else    
-            cout<<"NO"<<endl;
+        long long aModb = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            aModb = ((10*aModb) + (int)(s[i]-'0'))%41;
+        }
+        if(aModb == 0)
+            cout<<"YES";
+        else 
+            cout<<"NO";
+        cout<<endl;
     }
     return 0;
 }
--- Coding-Ninjas/Assignment-5-Mixed/word_break-1.cpp
+++ Coding-Ninjas/Assignment-5-Mixed/word_break-1.cpp
@@ -1,17 +1,41 @@
 #include <bits/stdc++.h>
+#include <map>
 using namespace std;
+map<string,int> dict;
+
+void wordbreak(string s,int n,string output)
+{
+    for(int i = 1; i <= n ; i++)
+    {
+        string prefix = s.substr(0,i);
+        if(dict.find(prefix)!=dict.end())
+        {
+            if(i == n)
+            {
+                output += prefix;
+                cout<<output<<endl;
+                return;
+            }
+            wordbreak(s.substr(i,n-i),n-i,output + prefix + " ");
+        }
+    }
+}
 
 int main()
 {
     int n;
     cin >> n;
 
-    vector<string> words(n);
     for(int i = 0; i < n ; i++)
-        cin >> words[i];
+    {
+        string a;
+        cin >> a;
+        dict[a]++;
+    }
+
     string s;
     cin >> s;
-
+    wordbreak(s,s.size(),"");
     
     return 0;
 }
