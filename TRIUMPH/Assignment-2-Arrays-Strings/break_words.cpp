7a62c160acc3d1df14dec09cebccab69f6e6665a
amrithm98 <amrithchallenger@gmail.com>
2018-02-20 18:17:23 +0530

    BREAK words. Assgn 2 Completed

--- TRIUMPH/Assignment-2-Arrays-Strings/break_words.cpp
+++ TRIUMPH/Assignment-2-Arrays-Strings/break_words.cpp
@@ -3,26 +3,55 @@ using namespace std;
 
 void breakWords(char* S)
 {
-    int len = strlen(S);
-
+    int len = strlen(S)-1;
     int count = 0;
+
     for(int i = len-1; i >= 0; i--)
     {
-        if(arr[i] == ' ')
+        // cout << i << " : " << count << endl;
+        if(S[i] == ' ')
         {
             if(count % 2 != 0)
             {
+                count = 0;
                 continue;
             }
             else
             {
-                
+                if(count >= 4)
+                {
+                    int start = (2 + i + count/2);
+                    int j;
+                    for( j = len; j >= start; j--)
+                    {
+                        S[j] = S[j-1];
+                    }
+                    S[j] = ' ';
+                    S[len+1] = '\0';
+                    len++;
+                }
             }
             count = 0;
         }
         else
             count++;
     }
+    if(count % 2 == 0 && count >= 4)
+    {
+        int start = (1 + count/2);
+        int j;
+        for( j = len; j >= start; j--)
+        {
+            S[j] = S[j-1];
+        }
+        S[j] = ' ';
+        S[len+1] = '\0';
+        len++;
+    }
+
+    for(int i = 0; i < len; i++)
+        cout << S[i];
+
 }
 
 int main()
