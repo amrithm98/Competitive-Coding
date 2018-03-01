12a80fc23f8ea933d0e698b6570047a97d88b1fd
amrithm98 <amrithchallenger@gmail.com>
2018-03-01 13:50:00 +0530

    LIS NLOGN

---
+++ Coding-Ninjas/Assignment-6-1-DP/coin_tower.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int getWays(int n,int x, int y)
+{
+    
+}
+
+string solve(int n, int x, int y)
+{
+	// Write your code here .
+    int sol = getWays(n,x,y);
+    
+}
+
+int main()
+{
+    int n,x,y;
+    cin >> n >> x >> y;
+    cout << sovle(n,x,y) << endl;
+    return 0;
+}
---
+++ Coding-Ninjas/Assignment-6-1-DP/lis_nlogn.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void lisOptim(int *arr, int n)
+{
+    vector<int> tail;
+    tail.push_back(arr[0]);
+
+
+    for(int i = 1; i < n; i++)
+    {
+        int val = arr[i];
+
+        if(val < tail[0])
+        {
+            tail[0] = val;
+        }
+        else if(val > tail[tail.size()-1])
+        {
+            tail.push_back(val);
+        }
+        else
+        {
+            int ceilIndex = lower_bound(tail.begin(),tail.end(),val) - tail.begin();
+            tail[ceilIndex] = val;
+        }
+    }
+
+    cout << tail.size() << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    lisOptim(arr,n);
+    return 0;
+}
